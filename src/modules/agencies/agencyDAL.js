b149ea524eff173eb9f0e9cfcb8d14e1174cdba7
Neda Najafi <n_unknown57@yahoo.com>
2022-07-13 15:16:37 +0430

    Update agencyDAL.js

--- src/modules/agencies/agencyDAL.js
+++ src/modules/agencies/agencyDAL.js
@@ -2,13 +2,14 @@ const AGENCY_MODEL = require('./agencyModel');
 
 const agencyDispatchTable = {
     INSERT: (data) => AGENCY_MODEL.create(data),
+    INSERT_PIECE: (id, data) => AGENCY_MODEL.findOneAndUpdate({ _id: id }, { $push: { pieces: data } }, { new: true }).exec(),
     UPDATE: (data) => AGENCY_MODEL.findOneAndUpdate({ _id: data.id }, data, { new: true }).exec(),
-    UPDATE_BY_UNIQUEID: (data) => AGENCY_MODEL.findOneAndUpdate({ uniqueId: data.uniqueId }, data, {new: true}).exec(),
-    UPDATEVEHICLES: (data) => AGENCY_MODEL.findOneAndUpdate({ _id: data.id }, { $push: { vehicles: data.vehicles } }, {new: true}).exec(),
+    UPDATE_BY_UNIQUEID: (data) => AGENCY_MODEL.findOneAndUpdate({ uniqueId: data.uniqueId }, data, { new: true }).exec(),
+    UPDATEVEHICLES: (data) => AGENCY_MODEL.findOneAndUpdate({ _id: data.id }, { $push: { vehicles: data.vehicles } }, { new: true }).exec(),
     REMOVE: (id) => AGENCY_MODEL.findByIdAndDelete(id).exec(),
     FIND_BY_ID: (id) => AGENCY_MODEL.findById(id).lean().exec(),
-    FIND_BY_UNIQUEID: (uniqueId) => AGENCY_MODEL.findOne({uniqueId: uniqueId}).lean().exec(),
-    FIND_BY_USERNAME: (username) => AGENCY_MODEL.findOne({username: username}).lean().exec(),
+    FIND_BY_UNIQUEID: (uniqueId) => AGENCY_MODEL.findOne({ uniqueId: uniqueId }).lean().exec(),
+    FIND_BY_USERNAME: (username) => AGENCY_MODEL.findOne({ username: username }).lean().exec(),
     GET_VEHICLES: (id) => AGENCY_MODEL.findById(id).select({ _id: 0, vehicles: 1 }).lean().exec(),
     FIND: () => AGENCY_MODEL.find().lean().exec()
 }
